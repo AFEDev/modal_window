70ba045c7526fdbf6cf049587b301095e562ad30
AFEDev <algirduj@gmail.com>
2022-05-17 00:21:47 +0300

    basic  modal window

---
+++ plugins/modal.js
@@ -0,0 +1,60 @@
+function _createModal(options) {
+    const modal = document.createElement('div')
+    modal.classList.add('amodal')
+    modal.insertAdjacentHTML("afterbegin", `
+    <div class="modal-overlay">
+        <div class="modal-window">
+            <div class="modal-header">
+                <span class="modal-title">${title}</span>
+                <span class="modal-close">&times;</span>
+            </div>
+            <div class="modal-body">
+              ${content}
+            </div>
+            <div class="modal-footer">
+                <button>Ok</button>
+                <button>Cancel</button>
+            </div>
+        </div>
+    </div>
+    `)
+    document.body.appendChild(modal)
+return modal;
+}
+
+/*  obj options:
+title:string
+closable: boolean xsui
+width: string
+destroy: remove modal window
+window close on overlay
+-------------------------
+set sontent(html:string):void | public
+onClose():void
+onOpen():void
+beforeClose(): boolean
+animate css 
+*/
+
+$.modal = function(options) {
+    const ANIMATION_SPEED = 200;
+    const $modal = _createModal(options);
+    let closing = false;
+
+    return {
+      open() {
+        !closing && $modal.classList.add('open')
+      },
+      close() {
+          closing = true;
+        $modal.classList.remove('open')
+        $modal.classList.add('hide')
+        setTimeout(()=>{
+            $modal.classList.remove('hide')
+            closing = false;
+        }, ANIMATION_SPEED)
+      },
+      destroy() {}
+    }
+    
+}
