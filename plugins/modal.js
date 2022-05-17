68b9b86089326f2a8c8594c71a1d760b9f27bef4
AFEDev <algirduj@gmail.com>
2022-05-17 22:59:37 +0300

    modal finished

--- plugins/modal.js
+++ plugins/modal.js
@@ -1,48 +1,70 @@
+Element.prototype.appendAfer= function (element) {
+  element.parentNode.insertBefore(this, element.nextSibling)
+}
+
+function noop() {}
+
+function _createModalFooter(buttons = []) {
+  if (buttons.length === 0) {
+    return document.createElement('div')
+  }
+  const wrap = document.createElement("div")
+    wrap.classList.add('modal-footer')
+
+    buttons.forEach(btn => {
+      const $btn = document.createElement('button')
+      $btn.textContent = btn.text
+      $btn.classList.add('btn')
+      $btn.classList.add(`btn-${btn.type || 'secondary'}`)
+      $btn.onclick = btn.handler || noop
+      wrap.appendChild($btn)
+    })
+  
+    return wrap
+};
+
 function _createModal(options) {
+  const DEFAULT_WIDTH = '600px'
     const modal = document.createElement('div')
     modal.classList.add('amodal')
     modal.insertAdjacentHTML("afterbegin", `
-    <div class="modal-overlay">
-        <div class="modal-window">
+    <div class="modal-overlay" data-close="true">
+        <div class="modal-window slide-out-bck-center" style="width:${options.width || DEFAULT_WIDTH}">
             <div class="modal-header">
-                <span class="modal-title">${title}</span>
-                <span class="modal-close">&times;</span>
+                <span class="modal-title">${options.title}</span>
+                ${options.closable ? `<span class="modal-close" data-close="true">&times;</span>`: ""}
             </div>
-            <div class="modal-body">
-              ${content}
-            </div>
-            <div class="modal-footer">
-                <button>Ok</button>
-                <button>Cancel</button>
+            <div class="modal-body" data-content>
+              ${options.content || ""}
             </div>
         </div>
     </div>
     `)
+    const footer = _createModalFooter(options.footerButton)
+footer.appendAfer(modal.querySelector('[data-content]'))
     document.body.appendChild(modal)
 return modal;
-}
+};
 
-/*  obj options:
-title:string
-closable: boolean xsui
-width: string
-destroy: remove modal window
-window close on overlay
--------------------------
-set sontent(html:string):void | public
-onClose():void
-onOpen():void
-beforeClose(): boolean
-animate css 
-*/
+//* onClose():void
+// onOpemn():void
+// beforeClouse():boolear
+//----------------
+//animame css
+
+
 
 $.modal = function(options) {
     const ANIMATION_SPEED = 200;
     const $modal = _createModal(options);
     let closing = false;
+    destroyed = false;
 
-    return {
+   const modal = {
       open() {
+        if (destroyed) {
+          return console.log('Modal is destroyed');
+        }
         !closing && $modal.classList.add('open')
       },
       close() {
@@ -54,7 +76,25 @@ $.modal = function(options) {
             closing = false;
         }, ANIMATION_SPEED)
       },
-      destroy() {}
     }
-    
-}
+
+    const listener = event => {
+      if (event.target.dataset.close) {
+        modal.close()
+    }
+  };
+
+    $modal.addEventListener('click', listener);
+
+    return Object.assign(modal, {
+      destroy() {
+        $modal.parentNode.removeChild($modal)
+        $modal.removeEventListener('click', listener)
+        destroyed = true;
+      },
+      setContent (html) {
+        $modal.querySelector('[data-content]').innerHTML = html
+      }
+    })
+  
+};
